1fadeafbfe629c322d2829304f9668095a30a560
polysyllabic <karl@polysyllabic.com>
2022-05-22 09:25:33 -0700

    add license header

--- chaosface/chaosface/chatbot/chatbot.py
+++ chaosface/chaosface/chatbot/chatbot.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+#-----------------------------------------------------------------------------
+# Twitch Controls Chaos (TCC)
+# Copyright 2021-2022 The Twitch Controls Chaos developers. See the AUTHORS
+# file at the top-level directory of this distribution for details of the
+# contributers.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#-----------------------------------------------------------------------------
 
 import sys # just for argument passing for bare test
 import socket
