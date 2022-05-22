ec9eb0a5a226185a5d7ee7cebd1097059f25e60a
polysyllabic <karl@polysyllabic.com>
2022-05-22 10:30:06 -0700

    start converting to new chatbot

--- chaosface/chaosface/TwitchControlsChaos.py
+++ chaosface/chaosface/TwitchControlsChaos.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+#-----------------------------------------------------------------------------
+# Twitch Controls Chaos (TCC)
+# Copyright 2021-2022 The Twitch Controls Chaos developers. See the AUTHORS
+# file at the top-level directory of this distribution for details of the
+# contributers.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#-----------------------------------------------------------------------------
 
 import threading
 import asyncio
@@ -7,7 +26,7 @@ import logging
 from flexx import flx
 
 from config import relay
-import chatbot.chatbot
+from chatbot.ChaosBot import ChaosBot
 
 from gui import ActiveMods, VoteTimer, Votes, Interface
 
@@ -15,33 +34,32 @@ import chaosface.ChaosModel as ChaosModel
 
 logging.basicConfig(level="INFO")
 
-		
+    
 def startFlexx():
-	flx.App(ActiveMods, relay).serve()
-	flx.App(VoteTimer, relay).serve()
-	flx.App(Votes, relay).serve()
-	flx.App(Interface, relay).serve()
-	
-	flx.create_server(host='0.0.0.0', port=relay.uiPort, loop=asyncio.new_event_loop())
-	flx.start()
+  flx.App(ActiveMods, relay).serve()
+  flx.App(VoteTimer, relay).serve()
+  flx.App(Votes, relay).serve()
+  flx.App(Interface, relay).serve()
+  
+  flx.create_server(host='0.0.0.0', port=relay.uiPort, loop=asyncio.new_event_loop())
+  flx.start()
 
 if __name__ == "__main__":
-	# for chat
-	chatbot = chatbot.Chatbot()
-	chatbot.setIrcInfo(relay.ircHost, relay.ircPort)
-	chatbot.start()
-	
-	#startFlexx()
-	flexxThread = threading.Thread(target=startFlexx)
-	flexxThread.start()
+  # for chat
+  logging.info("Starting chaos chatbot")
+  ChaosBot().run_threaded()
+  
+  #startFlexx()
+  flexxThread = threading.Thread(target=startFlexx)
+  flexxThread.start()
 
-	# Voting model:
-	chaosModel = ChaosModel(chatbot)
-	if (not chaosModel.process()):
-		chaosModel.print_help()
-			
-	logging.info("Stopping threads...")
-	
-	flx.stop()
-	flexxThread.join()
+  # Voting model:
+  chaosModel = ChaosModel(chatbot)
+  if (not chaosModel.process()):
+    chaosModel.print_help()
+      
+  logging.info("Stopping threads...")
+  
+  flx.stop()
+  flexxThread.join()
 
