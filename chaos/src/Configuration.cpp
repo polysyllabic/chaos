73de89ce0755f7bf1488d78c902de154f56f4a84
polysyllabic <karl@polysyllabic.com>
2022-06-05 15:26:01 -0700

    allow sniffify messages to go to a separate file

--- chaos/src/Configuration.cpp
+++ chaos/src/Configuration.cpp
@@ -31,7 +31,7 @@
 #include "GameMenu.hpp"
 
 // This lets us configure the severity level of log messages coming from usb-sniffify separately
-extern "C" void initializeSniffifyLog(plog::Severity severity, plog::IAppender* appender);
+extern "C" void initializeSniffifyLog(plog::Severity severity, std::string filename);
 
 using namespace Chaos;
 
@@ -63,26 +63,30 @@ Configuration::Configuration(const std::string& fname) {
     }
   }
 
-  // Set up the log
+  // Set up the main log
   std::filesystem::path logfile = configuration["log_file"].value_or("chaos.log");  
   logfile = log_path / logfile;
   
   bool overwrite = configuration["overwrite_log"].value_or(false);
 
   plog::Severity maxSeverity = (plog::Severity) configuration["log_verbosity"].value_or(3);
-  plog::Severity sniffifySeverity = (plog::Severity) configuration["sniffify_verbosity"].value_or(3);
 
   size_t max_size = (size_t) configuration["max_log_size"].value_or(0);
   int max_logs = configuration["max_log_files"].value_or(8);  
   if (overwrite && std::filesystem::exists(logfile)) {
     std::filesystem::remove(logfile);
   }
-  // Initialize the main logger
   plog::init(maxSeverity, logfile.c_str(), max_size, max_logs);
-  // Chain the sniffify logger into this one with its own severity level
-  initializeSniffifyLog(sniffifySeverity, plog::get());
-
   PLOG_NONE << "Welcome to Chaos " << CHAOS_VERSION;
+
+  std::filesystem::path sniflog = configuration["sniffify_log"].value_or("");
+  sniflog = log_path / sniflog;
+
+  plog::Severity sniffifySeverity = (plog::Severity) configuration["sniffify_verbosity"].value_or(3);
+  // IF we've specified a separate sniffify log, set it up
+  if (! sniflog.string().empty()) {
+    initializeSniffifyLog(sniffifySeverity, sniflog);
+  }
 
   interface_addr = configuration["interface_addr"].value_or("localhost");
   interface_port = configuration["interface_port"].value_or(5556);
