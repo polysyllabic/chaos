13663c48995bc823f2a88a7e90047ee2cb36deb3
polysyllabic <karl@polysyllabic.com>
2022-06-13 09:06:32 -0700

    test app without chatbot

---
+++ chaosface/tests/TwitchControlsChaos.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+  Twitch Controls Chaos (TCC)
+  Copyright 2021-2022 The Twitch Controls Chaos developers. See the AUTHORS
+  file at the top-level directory of this distribution for details of the
+  contributers.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import asyncio
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+from flexx import flx
+
+import chaosface.config.globals as config
+
+from chaosface.chatbot.chatbot import Chatbot
+from chaosface.gui.ActiveMods import ActiveMods
+from chaosface.gui.CurrentVotes import CurrentVotes
+from chaosface.gui.VoteTimer import VoteTimer
+from chaosface.gui.ChaosInterface import ChaosInterface
+
+
+from chaosface.ModifierSelection import ModifierSelection
+
+if __name__ == "__main__":
+  # for chat
+  #chatbot = Chatbot()
+  #chatbot.setIrcInfo(model.get_attribute('irc_host'), model.get_attribute('irc_port'))
+  #chatbot.start()
+  
+  # Set up GUI
+  interface = flx.App(ChaosInterface)
+  interface.serve('Chaos')
+  flx.App(ActiveMods).serve()
+  flx.App(VoteTimer).serve()
+  flx.App(CurrentVotes).serve()
+  flx.create_server(host='0.0.0.0', port=config.relay.get_attribute('ui_port'), loop=asyncio.new_event_loop())
+
+  # Event loop to select the modifiers and communicate with the chaos engine
+  selector = ModifierSelection()
+  selector.start()
+
+  # Start the browser-based GUI
+  flx.start()
+
