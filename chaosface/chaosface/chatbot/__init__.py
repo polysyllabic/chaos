06d71bae332621ce60b113a8061cceacd41ac56d
polysyllabic <karl@polysyllabic.com>
2026-02-11 10:55:01 -0800

    Clean up legacy code

--- chaosface/chaosface/chatbot/__init__.py
+++ chaosface/chaosface/chatbot/__init__.py
@@ -1,7 +1,3 @@
-from .chatbot import Chatbot
 from .ChaosBot import ChaosBot
 from .ChaosBot import ChaosBotContext
 from .context import RelayBotContext
-from .utility import chat
-from .utility import ban
-from .utility import timeout
