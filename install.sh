a54a5a6a913c846f02e9dc9b6a584a895ba66a7a
polysyllabic <karl@polysyllabic.com>
2026-03-16 21:31:56 -0700

    fix broken reconfiguration on yes response

--- install.sh
+++ install.sh
@@ -189,6 +189,10 @@ confirm_reboot() {
 }
 
 install_questions() {
+  remote_ui=0
+  interface_addr="localhost"
+  is_developer=0
+
   echo "Install chatbot and user interface on a separate computer?"
   select yn in "Yes" "No"; do
     case "${yn}" in
@@ -593,6 +597,7 @@ if (( install_stage >= 6 )); then
       Yes)
         install_stage=0
         selected_build_branch=""
+        break
         ;;
       No)
         exit
